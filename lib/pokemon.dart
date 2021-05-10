c997614ce1a1cd52af84506e806b6dfca45cc284
Tsilva16 <tisolesilva@gmail.com>
2021-05-10 20:20:48 -0300

    Add Abilities Poke

--- lib/pokemon.dart
+++ lib/pokemon.dart
@@ -1,9 +1,27 @@
 class Pokemon {
   String nome;
   String urlImage;
+  List <Ability> abilities;
 
   Pokemon.fromJson(Map<String, dynamic> json){
     nome = json['name'];
     urlImage = json['sprites']['front_default'];
+
+
+    var arrayAbilities = json['abilities'] as List;
+
+    abilities = arrayAbilities.map((item) {
+      return Ability.fromJson(item['ability']);
+    }).toList();
+  }
+}
+
+class Ability {
+  String name;
+  String url;
+
+  Ability.fromJson(Map<String, dynamic> json){
+    name = json['name'];
+    url = json['url'];
   }
 }
